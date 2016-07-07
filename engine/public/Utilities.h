959ce7d0890f9464fb213e6afe25ff9ce87bf38b
bno1 <ealex95@gmail.com>
2016-07-07 11:15:29 +0300

    utilities header file for the engine

---
+++ engine/public/Utilities.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define RUTIME_ASSERT(expression)                         \
+	if (expression) {                                     \
+		fprintf(stderr, "RUTNIME_ASSERT failed in function %s (%s:%d)\n", __FUNCTION__, __FILE__, __LINE__); \
+		exit(1);                                          \
+	}
