f1a8df62360184c4bd09995ad13fbec77be413db
bno1 <ealex95@gmail.com>
2016-07-12 19:21:23 +0300

    fixed RUNTIME_ASSERT behaviour

--- engine/public/Utilities.h
+++ engine/public/Utilities.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 #define RUTIME_ASSERT(expression)                         \
-	if (expression) {                                     \
+	if (!(expression)) {                                     \
 		fprintf(stderr, "RUTNIME_ASSERT failed in function %s (%s:%d)\n", __FUNCTION__, __FILE__, __LINE__); \
 		exit(1);                                          \
 	}
